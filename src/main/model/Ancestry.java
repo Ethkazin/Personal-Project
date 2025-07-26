3e0d490ebdf8c7aa66572fd9e2fd4f90e388ae0a
Paul Bunuan <pbunuan@students.cs.ubc.ca>
2025-07-26 13:19:05 -0700

    Changelog v0.1:
    - added ancestry class

---
+++ src/main/model/Ancestry.java
@@ -0,0 +1,17 @@
+package main.model;
+
+import java.util.ArrayList;
+
+// 
+public class Ancestry {
+    ArrayList<String> ancestries = new ArrayList<>();
+    String ancestry;
+    public ArrayList<String> Ancestry() {
+        return ancestries;
+    }
+    
+    public String getAncestry(String ancestry) {
+        return ancestry;
+    }
+
+}
