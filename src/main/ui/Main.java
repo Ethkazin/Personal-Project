7da38288f4a00dc1c8b58d80878fb75b3735f26e
Paul Bunuan <pbunuan@students.cs.ubc.ca>
2025-07-29 13:29:13 -0700

    Changelog v0.1.1:
    - added more to ancestry Class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,23 @@
 package main.ui;
 
+import java.util.ArrayList;
+import main.model.*;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Ancestry> ancestries = new ArrayList<>();
+        Ancestry ancestry1 = new Ancestry("Elf","Dungeon Dragon 5e");
+        Ancestry ancestry2 = new Ancestry("Dwarf", "Dungeon Dragon 5e");
+
+        ancestries.add(ancestry1);
+        ancestries.add(ancestry2);
+
+        for (Ancestry ancestry : ancestries){
+            System.out.println(ancestry.getName());
+        }
         
+
+
     }
 
 }
